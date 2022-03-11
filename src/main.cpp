59a74671dd81667f203c6ed0225b51adbbc455dc
Recelis <jackymlui@live.com>
2022-03-11 12:55:45 +1000

    installing libraries and setup

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <SPI.h>          // f.k. for Arduino-1.5.2
+#include <Adafruit_I2CDevice.h>
+#include "Adafruit_GFX.h"// Hardware-specific library
+#include <MCUFRIEND_kbv.h>
+#include <string.h>
+#include <ArduinoJson.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
